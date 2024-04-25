96be871af518c4ef9c719b91c2ee4bb43ecc0a7a
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-04-25 14:41:59 +0000

    Initial commit

--- com/example/RecPow.java
+++ com/example/RecPow.java
@@ -1,8 +1,13 @@
 
 package com.example;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 /**
- * Change me.
+ * Recursively calculates powers.
  *
  * @author Christopher Di Bert
  * @version 1.0
@@ -23,6 +28,59 @@ public final class RecPow {
      * @param args Unused
      */
     public static void main(final String[] args) {
-        System.out.println("Hello, world");
+        // Creates the input and output file paths.
+        final File inputPath = new File("/home/vscode/ICS4U/Unit3/Unit3-04/Unit3-04-Java-Power/com/example/input.txt");
+        final File outputPath = new File("/home/vscode/ICS4U/Unit3/Unit3-04/Unit3-04-Java-Power/com/example/output.txt");
+
+        try {
+        // Creates scanner and writer.
+        Scanner sc = new Scanner(inputPath);
+        FileWriter fileWriter = new FileWriter(outputPath);
+        BufferedWriter writer = new BufferedWriter(fileWriter);
+        while (sc.hasNext()) {
+            // Tries to cast the input to an int and pass it to RecFib.
+            try {
+                // Gets the integer value of the base.
+                int base = Integer.parseInt(sc.next());
+                // Gets the integer value of the exponent.
+                int exponent = Integer.parseInt(sc.next());
+                // Writes the fibonacci number to the output file.
+                writer.write(String.valueOf(RecursivePow(base, exponent)));
+                // If there wasn't an integer to be read from the input file.
+            } catch (Exception e) {
+            writer.write("Must enter an integer!");
+            }
+            // Creates a new line in the output file.
+            writer.newLine();
+            // Goes to the next line in the input file.
+            sc.nextLine();
+        }
+        sc.close();
+        writer.close();
+        // Error thrown if an invalid file path was defined.
+        } catch (Exception e) {
+        System.out.println("Invalid path(s)!");
+        }
     }
+
+  /**
+   * Method to recursively find the power of a number
+   *
+   * @param base passed.
+   * @param exponent passed.
+   * @return 1.
+   * @return base.
+   * @return base * RecursivePow(base, exponent - 1).
+   */
+    private static int RecursivePow(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else if (exponent == 1) {
+            return base;
+        }
+        else {
+            return base * RecursivePow(base, exponent - 1);
+        }
+    }
+
 }
